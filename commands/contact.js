ca7e93840a754ecf4ffe30acb744770c3dab80a8
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-11-16 16:51:07 -0500

    Create contact.js

---
+++ commands/contact.js
@@ -0,0 +1,14 @@
+module.exports.run = (bot, message, args, Discord) => {
+let jargs = args.join(" ")
+let res = new Discord.RichEmbed()
+.setTitle("Developer Contact")
+.setDescription("Contacting the bot's developer.")
+.addField(`Contacting Developer for reason:`, ` ${jargs} `)
+.setColor("GOLD")
+message.channel.send({embed: res})
+
+let dev = new Discord.RichEmbed()
+.setTitle(`Message from: ${message.author.username}`)
+.addField(`You were sent a message for reason:`, `${jargs}`)
+
+}

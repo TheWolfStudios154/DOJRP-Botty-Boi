6a5c0f6fde6dfc44615ef96a74ef765ee72b0a90
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-11-16 16:54:35 -0500

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -10,8 +10,9 @@ message.channel.send({embed: res})
 let dev = new Discord.RichEmbed()
 .setTitle(`Message from: ${message.author.username}`)
 .addField(`You were sent a message for reason:`, `${jargs}`)
-
 }
+bot.users.get("369256915479560192").send({embed: dev})
+
 module.exports.help = {
   name: "contact"
 }

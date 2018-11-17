25c4812a61eb3d456d06b1842dcf17b037f834a5
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-11-17 17:46:16 -0500

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,9 +2,9 @@ module.exports.run = (bot, message, args, Discord) => {
     let member = message.mentions.members.first();
   let emb = new Discord.RichEmbed()
   .setTitle("User info")
-  .setDescription(`Here is ${member.Username}'s user info.`)
+  .setDescription(`Here is ${member}'s user info.`)
   .addField("User Created at:", `${member.createdAt}`)
-  .addField("Username", `${member.Username}`)
+  .addField("Username", `${member}`)
 message.channel.send({embed: emb})
 }
 module.exports.help = {

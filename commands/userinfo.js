c404ba9b7917908b45c055952a110d3b3c5ca79b
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-11-17 17:28:12 -0500

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message, args, Discord) => {
+    let member = message.mentions.members.first();
+  let emb = new Discord.RichEmbed()
+  .setTitle("User info")
+  .setDescription(`Here is ${member.Username}'s user info.`)
+  .addField("User Created at:", `${member.createdAt}`)
+  .addField("Username", `${member.Username}`)
+
+}
